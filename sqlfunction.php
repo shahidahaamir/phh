c2bc23ab922659890699953d6bf78aad85d86a56
shahidahaamir <shahidahaamir@gmail.com>
2021-06-02 16:28:52 +0200

    ConnectionSQL

---
+++ sqlfunction.php
@@ -0,0 +1,26 @@
+<?php 
+function selecttable($query)
+{
+    $maDb = mysqli_connect('localhost','root',"",'magasin');
+    $res = mysqli_query($maDb, $query);
+
+    //if (!$res)return null; 
+    if ($res == false)return null;
+    //print_r ($res);
+   // echo '<br/>'.$res->num_rows.'<br/>';
+   
+    $rowcount = mysqli_num_rows($res);
+    //echo 'rows count: '.$rowcount."<hr/>";
+    //$row = mysqli_fetch_assoc($res); 
+    $returnarray = array();
+        while ($row = mysqli_fetch_assoc($res)) {
+        //print_r ($row) ;
+        //echo '<hr/>' ; 
+         array_push($returnarray, $row);
+        }
+    return  $returnarray;
+}   
+
+$monresultat = selecttable("select * from produits  ") ; 
+print_r ($monresultat) ; 
+?>
